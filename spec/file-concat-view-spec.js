f18369246b3108127fd3e18bace54fb0a672c74f
philipp-bous <wolf1bous@googlemail.com>
2018-03-13 23:07:18 +0100

    Base setup

---
+++ spec/file-concat-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FileConcat2View from '../lib/file-concat-view';
+
+describe('FileConcat2View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
